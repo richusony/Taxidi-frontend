34a391c531741c2049d9fe77e501de3f7c2c842b
Richu Sony <sonyrichu4@gmail.com>
2024-09-04 20:44:19 +0530

    fix: vehicle-image size

--- src/components/VehicleImageFullScreen.jsx
+++ src/components/VehicleImageFullScreen.jsx
@@ -18,11 +18,11 @@ const VehicleImageFullScreen = ({ vehicleImages, closeView }) => {
     }
 
     return (
-        <div className='transition-all ease-linear px-4 bg-black bg-opacity-80 absolute top-0 right-0 w-full h-screen'>
+        <div className='transition-all ease-linear px-4 bg-black bg-opacity-80 absolute top-0 right-0 w-full h-screen overflow-hidden'>
             <div className='mt-2 text-end h-fit'><h1 onClick={()=> closeView(false)} className='text-white font-bold cursor-pointer'>Close</h1></div>
-            <div className='mx-auto mt-20 md:mt-10 flex justify-between items-center'>
+            <div className='w-full h-full m-auto flex justify-between items-center'>
                 <button onClick={() => handleChangeImage("prev")}><FontAwesomeIcon icon={faAngleLeft} className='bg-white py-2 px-3 rounded-full' /></button>
-                <div className='md:w-1/2 md:h-1/2'>
+                <div className='md:w-2/3 md:h-4/5'>
                     <img className='w-full h-full object-cover rounded' src={vehicleImages[imageIndex]} alt="vehicle-image" />
                 </div>
                 <button onClick={() => handleChangeImage("next")}><FontAwesomeIcon icon={faAngleRight} className='bg-white py-2 px-3 rounded-full' /></button>
